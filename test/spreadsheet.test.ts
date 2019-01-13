97ff13bdd5b538a1d04872f0163abe1bb33e0ac6
Filipe Militão <fm2097ad@gmail.com>
2019-01-13 18:35:44 +0000

    Add remaining tests for spreadsheet get

--- test/spreadsheet.test.ts
+++ test/spreadsheet.test.ts
@@ -1,9 +1,11 @@
 import { Spreadsheet } from '../app/spreadsheet';
 import { sheets_v4 } from 'googleapis';
 import nock from 'nock';
+import { NestedError } from '../app/auth';
 
 describe('spreadsheet testing', () => {
 
+    // ensure will not try to use network
     beforeAll(() => nock.disableNetConnect());
     afterAll(() => nock.enableNetConnect());
 
@@ -15,26 +17,51 @@ describe('spreadsheet testing', () => {
         jest.resetAllMocks();
     });
 
-    test('throws error on not found', async () => {
-        const spreadsheetId = 'fake-id';
-
-        class FakeError extends Error {
-            response: { status: number };
-            constructor(message: string) {
-                super(message);
-                this.response = { status: 404 };
-            }
-        };
-
+    function mockApi(response: any): sheets_v4.Sheets {
         // @ts-ignore
-        const api = {
+        return {
             spreadsheets: {
-                get: () => Promise.reject(new FakeError('fake'))
+                get: () => response
             }
         } as sheets_v4.Sheets;
+    }
 
-        const result = await Spreadsheet.getExistingSpreadsheet(api, spreadsheetId);
-        expect(result).toBeNull();
+    class FakeError extends Error {
+        constructor(
+            message: string,
+            readonly response: any
+        ) {
+            super(message);
+        }
+    };
+
+    describe('Spreadsheet.getExistingSpreadsheet', () => {
+        const spreadsheetId = 'fake-id';
+
+        test('return null if spreadsheet not found (404)', async () => {
+            const api = mockApi(Promise.reject(new FakeError('fake-error', { status: 404 })));
+            const result = await Spreadsheet.getExistingSpreadsheet(api, spreadsheetId);
+            expect(result).toBeNull();
+        });
+
+        test('throws error if error that is not a 404', async () => {
+            const mockedError = new FakeError('fake-error', { status: 500 });
+            const api = mockApi(Promise.reject(mockedError));
+
+            try {
+                await Spreadsheet.getExistingSpreadsheet(api, spreadsheetId);
+                fail('Expecting to throw error');
+            } catch (error) {
+                expect(error).toBeInstanceOf(NestedError);
+                const nestedError = error as NestedError;
+                expect(nestedError.error).toBe(mockedError);
+            }
+        });
+
+        test('returns spreadsheet on correct result', async () => {
+            const api = mockApi(Promise.resolve({ data: 'ignore-me' }));
+            const spreadsheet = await Spreadsheet.getExistingSpreadsheet(api, spreadsheetId);
+            expect(spreadsheet).not.toBeNull();
+        });
     });
-
 });
