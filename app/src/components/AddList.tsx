b6a6d79f6d9e6710769bc0613820b6db9d44922a
Filipe Militão <fm2097ad@gmail.com>
2019-02-02 20:36:27 +0000

    Fix styles

--- app/src/components/AddList.tsx
+++ app/src/components/AddList.tsx
@@ -1,119 +1,121 @@
 import React from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
 
 import { Theme, withStyles, createStyles } from '@material-ui/core';
 
 type Props = {
-    // type of `fab` not really important, just to please typescript
-    classes: { fab: any },
-    onCreate: (name: string) => void,
+  // type of `fab` not really important, just to please typescript
+  classes: { fab: any },
+  onCreate: (name: string) => void,
 };
 
 type State = {
-    open: boolean,
-    value: string
+  open: boolean,
+  value: string
 };
 
-class AddListDialog extends React.Component<Props, State> {
-    constructor(props: Readonly<Props>) {
-        super(props);
-        this.state = {
-            open: false,
-            value: '',
-        };
-
-        this.handleOpen = this.handleOpen.bind(this);
-        this.handleCancel = this.handleCancel.bind(this);
-        this.handleCreate = this.handleCreate.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        if (event.target.value === null || event.target.value === undefined) {
-            return;
-        }
-        this.setState({ value: event.target.value });
-    }
-
-    handleOpen() {
-        this.setState({ open: true });
+/**
+ * Add List Fab button and Dialog
+ */
+class AddList extends React.Component<Props, State> {
+  constructor(props: Readonly<Props>) {
+    super(props);
+    this.state = {
+      open: false,
+      value: '',
     };
 
-    handleCreate() {
-        const listName = this.state.value;
-        this.props.onCreate(listName);
-        this.setState({ open: false });
-    };
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleCancel() {
-        this.setState({
-            open: false,
-            value: '',
-        });
-    };
-
-    render() {
-        const { classes } = this.props;
-        const canCreate = this.state.value.length <= 0;
-
-        return (
-            <div>
-                <Fab
-                    color='secondary'
-                    aria-label='Add Item'
-                    className={classes.fab}
-                    onClick={this.handleOpen}
-                >
-                    <AddIcon />
-                </Fab>
-
-                <Dialog
-                    open={this.state.open}
-                    onClose={this.handleCancel}
-                    aria-labelledby='form-dialog-title'
-                >
-                    <DialogTitle id='form-dialog-title'>New List</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Create a new shopping list.
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin='dense'
-                            id='name'
-                            label='List Name'
-                            type='text'
-                            placeholder='e.g. LIDL tuesday'
-                            fullWidth
-                            onChange={this.handleChange}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleCancel} color='primary'>Cancel</Button>
-                        <Button onClick={this.handleCreate} disabled={canCreate} color='primary'>Create</Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-        );
+  handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    if (event.target.value === null || event.target.value === undefined) {
+      return;
     }
+    this.setState({ value: event.target.value });
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  };
+
+  handleCreate() {
+    const listName = this.state.value;
+    this.props.onCreate(listName);
+    this.setState({ open: false });
+  };
+
+  handleCancel() {
+    this.setState({
+      open: false,
+      value: '',
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const canCreate = this.state.value.length <= 0;
+
+    return (
+      <div>
+        <Fab
+          color='secondary'
+          title='Add Item'
+          className={classes.fab}
+          onClick={this.handleOpen}
+        >
+          <AddIcon />
+        </Fab>
+
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleCancel}
+        >
+          <DialogTitle>New List</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Create a new shopping list.
+            </DialogContentText>
+            <TextField
+              onChange={this.handleChange}
+              autoFocus
+              margin='dense'
+              label='List Name'
+              type='text'
+              placeholder='e.g. LIDL tuesday'
+              fullWidth
+            />
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={this.handleCancel} color='primary'>Cancel</Button>
+            <Button onClick={this.handleCreate} disabled={canCreate} color='primary'>Create</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 }
 
 const styles = (theme: Theme) => createStyles({
-    fab: {
-        position: 'absolute',
-        bottom: theme.spacing.unit * 2,
-        right: theme.spacing.unit * 2,
-    }
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2,
+  }
 });
 
-const AddListDialogWithStyle = withStyles(styles)(AddListDialog);
+const AddListWithStyle = withStyles(styles)(AddList);
 
-export default AddListDialogWithStyle;
+export default AddListWithStyle;
--- app/src/components/List.tsx
+++ app/src/components/List.tsx
@@ -6,65 +6,65 @@ import { PoseGroup } from 'react-pose';
 import AddList from './AddList';
 
 type ListPropType = {
-    list: model.List,
-    add: (list: model.List) => void,
+  list: model.List,
+  add: (list: model.List) => void,
 };
 
 type ListStateType = {
-    items: model.Item[],
+  items: model.Item[],
 };
 
 const sortFunction = (a: model.Item, b: model.Item) => {
-    if (a.done === b.done) {
-        return a.uuid - b.uuid;
-    }
-    if (!a.done) {
-        return -1;
-    }
-    return 1;
+  if (a.done === b.done) {
+    return a.uuid - b.uuid;
+  }
+  if (!a.done) {
+    return -1;
+  }
+  return 1;
 };
 
 class List extends Component<ListPropType, ListStateType> {
-    constructor(props: Readonly<ListPropType>) {
-        super(props);
-        this.state = {
-            items: props.list.items.slice().sort(sortFunction),
-        };
-    }
+  constructor(props: Readonly<ListPropType>) {
+    super(props);
+    this.state = {
+      items: props.list.items.slice().sort(sortFunction),
+    };
+  }
 
-    onButtonPress() {
-        this.props.add(this.props.list);
-    }
+  onButtonPress() {
+    this.props.add(this.props.list);
+  }
 
-    onButtonPress2(name: string) {
-        console.log(name);
-    }
+  onButtonPress2(name: string) {
+    console.log(name);
+  }
 
-    // FIXME: horrible mess
-    updateItem() {
-        this.setState((prevState) => {
-            const sorted = prevState.items.slice();
-            sorted.sort(sortFunction);
-            return {
-                ...prevState,
-                items: sorted
-            };
-        });
-    }
+  // FIXME: horrible mess
+  updateItem() {
+    this.setState((prevState) => {
+      const sorted = prevState.items.slice();
+      sorted.sort(sortFunction);
+      return {
+        ...prevState,
+        items: sorted
+      };
+    });
+  }
 
-    render() {
-        const count = this.state.items.length;
-        const done = this.state.items.filter((item) => !item.done).length;
+  render() {
+    const count = this.state.items.length;
+    const done = this.state.items.filter((item) => !item.done).length;
 
-        return (<div className="List">
-            <div className='List-header'>{this.props.list.name}: {done} / {count}</div>
-            <PoseGroup>
-                {this.state.items.map((item) => (<Item key={item.uuid} item={item} inputChange={() => this.updateItem()} />))}
-            </PoseGroup>
+    return (<div className="List">
+      <div className='List-header'>{this.props.list.name}: {done} / {count}</div>
+      <PoseGroup>
+        {this.state.items.map((item) => (<Item key={item.uuid} item={item} inputChange={() => this.updateItem()} />))}
+      </PoseGroup>
 
-            <AddList onCreate={(name) => this.onButtonPress2(name)}/>
-        </div>);
-    }
+      <AddList onCreate={(name) => this.onButtonPress2(name)} />
+    </div>);
+  }
 }
 
 export default List;
