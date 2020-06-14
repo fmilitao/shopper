469dd105928f292dc061d9dc36c6803e9953d519
Filipe Militão <fm2097ad@gmail.com>
2020-06-14 09:57:47 +0100

    Updates

---
+++ precache-manifest.73a73390e90c680d9eb34f82b545cd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532181015fb4c120459173d7d55dcfd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e327e94de17032b48540",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c000e97d2f243d394a19",
+    "url": "/static/js/2.c1d8acfb.chunk.js"
+  },
+  {
+    "revision": "3431f5e0c47edfb64a1819e14e9faefa",
+    "url": "/static/js/2.c1d8acfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e327e94de17032b48540",
+    "url": "/static/js/main.59f777ab.chunk.js"
+  },
+  {
+    "revision": "537360cf3e6bca77b3c5",
+    "url": "/static/js/runtime-main.31542f62.js"
+  }
+]);
