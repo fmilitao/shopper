a4a7aed9e89b08c42008d3f4554a464d2027c07e
Filipe Militão <fm2097ad@gmail.com>
2021-11-16 21:25:02 +0000

    Updates

--- precache-manifest.e2d07dee5a6b016f47cfd76c8779e2d9.js
+++ precache-manifest.e2d07dee5a6b016f47cfd76c8779e2d9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a949f132489a162d4170fce17fdc204",
+    "revision": "2befc88932770cb881b9a15633aed27d",
     "url": "/shopper/index.html"
   },
   {
-    "revision": "939cc56abeb72b6c1ff5",
+    "revision": "0fa175b65aeafa5a4763",
     "url": "/shopper/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "004e5750f4efdf75fedd",
+    "revision": "6c36fc6aad0024cd181d",
     "url": "/shopper/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "939cc56abeb72b6c1ff5",
-    "url": "/shopper/static/js/2.19af6b7e.chunk.js"
+    "revision": "0fa175b65aeafa5a4763",
+    "url": "/shopper/static/js/2.61041d92.chunk.js"
   },
   {
     "revision": "b9de141b733ac0778cf818782945751e",
-    "url": "/shopper/static/js/2.19af6b7e.chunk.js.LICENSE.txt"
+    "url": "/shopper/static/js/2.61041d92.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "004e5750f4efdf75fedd",
-    "url": "/shopper/static/js/main.dd663984.chunk.js"
+    "revision": "6c36fc6aad0024cd181d",
+    "url": "/shopper/static/js/main.ea6c8bd8.chunk.js"
   },
   {
     "revision": "6999841b96ee181764d9",
