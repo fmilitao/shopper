92cf6253597b4a510260d3d86683d924974573f0
Filipe Militão <fm2097ad@gmail.com>
2022-01-06 20:11:27 +0000

    Updates

---
+++ precache-manifest.136ae45ef0af20199f26fb750e65baab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe97fbb01841af64764c4dc41f2d767",
+    "url": "/shopper/index.html"
+  },
+  {
+    "revision": "4fd779ad5b263c865ed6",
+    "url": "/shopper/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7e2e1397b05df15df1d3",
+    "url": "/shopper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fd779ad5b263c865ed6",
+    "url": "/shopper/static/js/2.64c7d6c5.chunk.js"
+  },
+  {
+    "revision": "8238b01ffd7b8130c1f1eb7126d02fd9",
+    "url": "/shopper/static/js/2.64c7d6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2e1397b05df15df1d3",
+    "url": "/shopper/static/js/main.131aa8ef.chunk.js"
+  },
+  {
+    "revision": "6999841b96ee181764d9",
+    "url": "/shopper/static/js/runtime-main.705dd939.js"
+  }
+]);
